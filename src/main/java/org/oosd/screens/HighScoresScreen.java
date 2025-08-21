7684e42ba23f0fd63975dbb6ddae82030b603829
Adam Jensen <jensen.adam@gmail.com>
2025-08-21 10:42:28 +0100

    added dummy high score data

--- src/main/java/org/oosd/screens/HighScoresScreen.java
+++ src/main/java/org/oosd/screens/HighScoresScreen.java
@@ -4,6 +4,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import org.oosd.Main;
@@ -20,16 +21,72 @@ public class HighScoresScreen {
     public void show() {
         VBox highScoresScreen = new VBox(10);
         highScoresScreen.setPadding(new Insets(20));
-        highScoresScreen.setAlignment(Pos.TOP_LEFT);
+
 
         Label label = new Label("High Scores");
+        label.setAlignment(Pos.TOP_CENTER);
+
+
+        //Dummy high score data
+        String[] names = {
+                "Alex",
+                "Sarah",
+                "Mike",
+                "Emma",
+                "James",
+                "Lisa",
+                "David",
+                "Sophie",
+                "Chris",
+                "Kate"
+        };
+
+        int[] scores = {
+                50000,
+                45000,
+                42000,
+                38000,
+                35000,
+                32000,
+                28000,
+                25000,
+                22000,
+                20000
+        };
+
+        //Grid to display scores
+        GridPane scoreGrid = new GridPane();
+        scoreGrid.setAlignment(Pos.CENTER);
+        scoreGrid.setHgap(20);
+        scoreGrid.setVgap(10);
+
+        //Column headers
+        Label rankLabel = new Label("Rank");
+        Label nameLabel = new Label("Name");
+        Label scoreLabel = new Label("Score");
+
+        scoreGrid.add(rankLabel, 0, 0);
+        scoreGrid.add(nameLabel, 1, 0);
+        scoreGrid.add(scoreLabel, 2, 0);
+
+        //Add scores
+        for (int i = 0; i < scores.length; i++) {
+            Label rank = new Label(String.valueOf(i + 1));
+            Label name = new Label(names[i]);
+            Label score = new Label(String.valueOf(scores[i]));
+
+            scoreGrid.add(rank, 0, i + 1);
+            scoreGrid.add(name, 1, i + 1);
+            scoreGrid.add(score, 2, i + 1);
+        }
+
 
         Button backButton = new Button("Back");
         backButton.setLayoutX(10);
         backButton.setLayoutY(10);
         backButton.setOnAction(e -> mainApp.showMainMenuScreen());
 
-        highScoresScreen.getChildren().addAll(label, backButton);
+        highScoresScreen.getChildren().addAll(label, scoreGrid, backButton);
         root.getChildren().setAll(highScoresScreen);
     }
 }
